22656574af9fb865b62a3526d285bb28433a6c35
Julian Camilo Garcia Escobar <juliancamilogarciaescobar7@gmail.com>
2024-03-02 20:59:20 -0500

    update connection bd - docker compose

--- back_cloud/database.py
+++ back_cloud/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432"
-#SQLALCHEMY_DATABASE_URL = "postgresql://example:example@db:5432/backcloud"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432"
+SQLALCHEMY_DATABASE_URL = "postgresql://example:example@db:5432/backcloud"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
